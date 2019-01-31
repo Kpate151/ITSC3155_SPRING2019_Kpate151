6b5c8ad9bd8e12a3d0538ed8acffd45bc6a48fe5
Your Name <you@example.com>
2019-01-31 15:34:04 +0000

    RubyBasicsl - Part 1

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -6,10 +6,46 @@ end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+ 
+  if(arr.length == 0)
+     return 0
+  elsif (arr.length == 1)
+     return arr[0]
+  else 
+     arr.sort!
+     num1 = 0
+     num1 = arr.max
+     arr.pop
+     num2 = 0
+     num2 = arr.max
+     return num1+num2 
+  end
 end
+
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  def sum_to_n? arr, n
+  if (arr.length == 0)
+    return false
+  end
+  if (arr.length == 1)
+    return false
+  end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
+    end
+  end
+  return false
+  end
 end
