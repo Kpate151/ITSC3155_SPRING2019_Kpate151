0a29dda0efe08e91222263ec1df1e83d89158cbb
Your Name <you@example.com>
2019-02-12 15:30:32 +0000

    Lab 9 initial commit

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -2,15 +2,25 @@
 
 # Part I
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
+
 
 # Part II
 def starts_with_consonant? s
-  # YOUR CODE HERE
+
+/^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
+
+
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+   if (/^[0-1]+$/.match(s)).nil?
+         return false end
+    a = s.to_i
+    if a % 4==0
+      return true
+    end
+    return false
 end
