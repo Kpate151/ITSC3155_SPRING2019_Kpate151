0cf4243a1284a49f07bbe656bae8d432f67cdc08
Your Name <you@example.com>
2019-04-09 07:02:18 +0000

    Add error messages for title validations

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,16 +3,28 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end 
     
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new 
+        @article = Article.new
+    end
+    
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
-    end 
+        if @article.save
+            redirect_to @article
+        else 
+            render 'new'
+        end 
+    end
 end
-
+def new
+    @article = Article.new
+end
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
