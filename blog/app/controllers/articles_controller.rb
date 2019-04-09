859224b63c9aa306c29ee67204a5e7380d0f5ad0
Your Name <you@example.com>
2019-04-09 04:19:27 +0000

    Set up the create action

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,9 @@
+class AtticlesController < ApplicationController
+    def new
+    end 
+    
+    def create
+        render plain: params[:article].inspect
+    end 
+end
+
--- blog/app/controllers/atticles_controller.rb
+++
@@ -1,5 +0,0 @@
-class AtticlesController < ApplicationController
-    def new
-    end 
-end
-
